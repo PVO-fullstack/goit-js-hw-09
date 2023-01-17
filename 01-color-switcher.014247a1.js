ac50cac260c2c698bdb2dece557882ef40621bfb
PVO-fullstack <113464187+PVO-fullstack@users.noreply.github.com>
2023-01-17 12:36:14 +0000

    Deploying to gh-pages from @ PVO-fullstack/goit-js-hw-09@b1a6e60fe7bc1b0eec06c242e93f90588e20aba4 🚀

--- 01-color-switcher.014247a1.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");e.disabled=!0,t.addEventListener("click",(function(){timerId=setInterval((function(){return t="#".concat(Math.floor(16777215*Math.random()).toString(16)),void(d.style.backgroundColor=t);var t}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(timerId),e.disabled=!0,t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.014247a1.js.map
